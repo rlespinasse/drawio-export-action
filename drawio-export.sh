b7d0808521a32f1c0b20878e524926c7aafb4e48
Romain Lespinasse <romain.lespinasse@gmail.com>
2023-05-02 23:32:06 +0200

    cleanup

--- drawio-export.sh
+++ drawio-export.sh
@@ -50,9 +50,11 @@ fi
 
 # Need of full clone execpt for 'all' action mode
 if [ "${INPUT_ACTION_MODE}" != "all" ]; then
+  # To correctly configure git setup inside a container
+  # See https://github.com/actions/checkout/issues/1169
+  git config --system --add safe.directory '/github/workspace'
   # Need a classic clone of the repository to work with
   # but 'actions/checkout' make a shallow clone by default$
-  git config --system --add safe.directory '*'
   if [ "$(git rev-parse --is-shallow-repository)" == "true" ]; then
     error_message="This is a shallow git repository."
     if [ -f "$GITHUB_OUTPUT" ]; then
